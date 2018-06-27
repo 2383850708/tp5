c39469976b9275b91ecd3311e2516f1a96ed92ad
wenyuankun <2383850708@qq.com>
2018-06-27 17:17:53 +0800

    2.1

--- application/admin/controller/article/Article.php
+++ application/admin/controller/article/Article.php
@@ -13,7 +13,18 @@ class Article extends Backend
 	{
 		if($this->request->isAjax())
 		{
-			print_r($_REQUEST);exit;
+			$data = input('param.');
+			$info = $this->upload();
+			if($info['status']==1)
+			{
+				$data['pic'] = $info['path'];
+			}
+			else if($info['status']==0)
+			{
+				return parent::returnJson($info['path'],0);
+			}
+			
+			
 		}
 		else
 		{
@@ -23,6 +34,31 @@ class Article extends Backend
 			return $this->fetch();
 		}
 		
+	}
+
+	public function upload()
+	{
+		$file = $this->request->file('file');
+			$data = array();
+			$data['status'] = 3;
+			if($file)
+			{
+		        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+		        if($info)
+		        {
+		        	$data['status'] = 1;
+		        	$data['path'] = $info->getSaveName();;
+		        	// $image = \think\Image::open($info->getPathName());
+					// 给原图左上角添加水印并保存water_image.png
+					//$image->text('www.baidu.com','fontlibrary/Lucia.ttf',20,'#ffffff')->save($info->getPathName());
+		        }
+		        else
+		        {
+		            $data['status'] = 0;
+		        	$data['path'] = $file->getError();
+		        }
+		    }
+		    return $data;
 	}
 
 	
